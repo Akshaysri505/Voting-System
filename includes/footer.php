0aebbd11e6835f8a5320cd0129abaad8628e7a15
Akshaysri505 <80389692+Akshaysri505@users.noreply.github.com>
2022-10-03 13:49:32 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2021 Brought To You By Group No. 23</strong>
+    </div>
+    <!-- /.container -->
+</footer>
